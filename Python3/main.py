c4cc636e41e23a49b052dcf5b6147c54804109b7
Treayn <Treayn@gmail.com>
2018-03-29 00:39:16 -0400

    Stuff

--- Python3/main.py
+++ Python3/main.py
@@ -1,22 +1,22 @@
 import asyncio
-from camera import Camera
+from cube_vision import CubeVision
 from socket import *
 
 def spinImage(cam, lo, sock):
     errorVal = cam.process()
     print(errorVal)
-    sock.sendall(str(errorVal).encode("UTF-8"))
+    #sock.sendall(str(errorVal).encode("UTF-8"))
     lo.call_later(0.1, spinImage, cam, lo, sock)
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
 
     # Create objects
-    camera = Camera(1650, 1080)
+    camera = CubeVision(0, 1650, 1080)
     
     # Create socket
     datagram = socket(AF_INET, SOCK_DGRAM)
-    datagram.connect(("192.168.43.113", 10000))
+    # datagram.connect(("192.168.43.113", 10000))
 
     try:
         # Schedule calls
