e74ec95abd95994920e8f6d2c547ce8d95780f4c
Eric <Treayn@gmail.com>
2018-04-18 02:09:01 -0400

    Minor changes

--- cube_vision.py
+++ cube_vision.py
@@ -5,7 +5,7 @@ import time
 class CubeVision(object):
     def __init__(self, x_center: float, y_center: float):
         # Center of camera is our target position.
-        self.target_position = int(x_center)
+        self.target_position = x_center
         self.current_position = self.target_position
         self.height = int(y_center)
 
@@ -20,8 +20,8 @@ class CubeVision(object):
     
     def _overlay_target(self, frame) -> None:
         """Add a green target circle on the GUI."""
-        cv2.circle(frame, (self.target_position, self.height), 7, (0, 255, 0), -1)
-        cv2.putText(frame, "Robot Center", (self.target_position, self.height - 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+        cv2.circle(frame, (int(self.target_position), self.height), 7, (0, 255, 0), -1)
+        cv2.putText(frame, "Robot Center", (int(self.target_position), self.height - 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
     
     def _threshold_image(self, frame) -> None:
         """Filter and clean image."""
@@ -44,11 +44,11 @@ class CubeVision(object):
     def _get_centers(self) -> None:
         """Get the contour moments/centers"""
         M = cv2.moments(self.largest_contour)
-        self.current_position = int(round(M['m10']/M['m00']))
+        self.current_position = round(M['m10']/M['m00'])
     
     def _overlay_actual(self, frame) -> None:
-        cv2.circle(frame, (self.current_position, self.height), 7, (0, 0, 255), -1)
-        cv2.putText(frame, "Target Position", (self.current_position, self.height + 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+        cv2.circle(frame, (int(self.current_position), self.height), 7, (0, 0, 255), -1)
+        cv2.putText(frame, "Target Position", (int(self.current_position), self.height + 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
     
     def _display_results(self, frame) -> None:
         cv2.imshow('Raw', frame)
@@ -65,15 +65,15 @@ class CubeVision(object):
 
         # Get the error, averaged over the last 4 samples.
         self.dx = np.mean(self.samples)
-        self.filtered_position = int(round(self.dx + self.target_position))
+        self.filtered_position = round(self.dx + self.target_position)
     
     def _print_results(self) -> None:
         print('Current position: ' + str(self.current_position))
         print('Target position: ' + str(self.target_position))
 
     def _overlay_filtered(self, frame) -> None:
-        cv2.circle(frame, (self.filtered_position, self.height), 7, (255, 0, 0), -1)
-        cv2.putText(frame, "Target Filtered", (self.filtered_position + 20, self.height), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
+        cv2.circle(frame, (int(self.filtered_position), self.height), 7, (255, 0, 0), -1)
+        cv2.putText(frame, "Target Filtered", (int(self.filtered_position) + 20, self.height), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
 
     def _get_error(self) -> float:
         return self.dx
@@ -84,8 +84,8 @@ class CubeVision(object):
             self._get_contours()
             self._get_centers()
         except (ValueError, ZeroDivisionError) as e:
-            # If no contours spotted, hold the current heading/position
-            pass
+            # If no contours spotted sustain current heading, but decay to zero error.
+            self.current_position -= min(50, max(-50, self.current_position - self.target_position))
         self._filter_results()
         self._print_results()
         #self._overlay_actual(frame)
